a6895c073d3ad8a40e6ee5765ab359e5342b0ff2
FraLippo <jeanfrancois.poivey@gmail.com>
2023-01-17 18:52:29 +0100

    modification publicité

--- ReactCoreCerebral/ClientApp/src/components/commun/adSense.js
+++ ReactCoreCerebral/ClientApp/src/components/commun/adSense.js
@@ -1,21 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+export default class Ad extends Component {
 
+    componentDidMount() {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+    render() {
+        return <ins className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-0014588513895125"
+            data-ad-slot="6880354109"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+    }
 
-export default function Ad()  {
-
-    return <React.Fragment><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0014588513895125"
-      crossOrigin="anonymous"></script>
-
-      <ins className="adsbygoogle"
-        style={{display:"block"}}
-        data-ad-client="ca-pub-0014588513895125"
-        data-ad-slot="6880354109"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({ });
-      </script>
-    </React.Fragment>
-
-  
 }

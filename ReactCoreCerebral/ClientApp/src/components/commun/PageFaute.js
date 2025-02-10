594144e7792e0a1bd4eaa11532da3dd938f5368a
FraLippo <jeanfrancois.poivey@gmail.com>
2025-02-10 10:14:55 +0100

    Nouveau message erreur

--- ReactCoreCerebral/ClientApp/src/components/commun/PageFaute.js
+++ ReactCoreCerebral/ClientApp/src/components/commun/PageFaute.js
@@ -1,6 +1,18 @@
-export default function pageFaute()
-{
-    return <div className="centre"><div >Désolé, nous avons rencontré un problème.</div>
-    <div><a href="https://orthographe.evalquiz.com">Retour à l'accueil</a></div>
+
+
+import { useRouteError } from "react-router-dom";
+
+function pageFaute() {
+  const error = useRouteError();
+
+  return (
+    <div>
+      <h1>Désolé, nous avons rencontré un problème.</h1>
+      <p>{error.status ? `Erreur ${error.status} : ${error.statusText}` : "Une erreur inconnue est survenue."}</p>
+      <pre>{error.message || JSON.stringify(error, null, 2)}</pre> {/* Affiche les détails */}
+      <div><a href="https://cerebral.evalquiz.com">Retour à l'accueil</a></div>
     </div>
-} 
+  );
+}
+
+export default {pageFaute}

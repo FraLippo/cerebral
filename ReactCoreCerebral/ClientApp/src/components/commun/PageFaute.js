5fa1ac9a046476caf38032f5726340ebd880a8a0
FraLippo <jeanfrancois.poivey@gmail.com>
2025-02-13 13:59:27 +0100

    correction page faute

--- ReactCoreCerebral/ClientApp/src/components/commun/PageFaute.js
+++ ReactCoreCerebral/ClientApp/src/components/commun/PageFaute.js
@@ -3,13 +3,10 @@
 import { useRouteError } from "react-router-dom";
 
 function pageFaute() {
-  const error = useRouteError();
 
   return (
     <div>
       <h1>Désolé, nous avons rencontré un problème.</h1>
-      <p>{error.status ? `Erreur ${error.status} : ${error.statusText}` : "Une erreur inconnue est survenue."}</p>
-      <pre>{error.message || JSON.stringify(error, null, 2)}</pre> {/* Affiche les détails */}
       <div><a href="https://cerebral.evalquiz.com">Retour à l'accueil</a></div>
     </div>
   );
